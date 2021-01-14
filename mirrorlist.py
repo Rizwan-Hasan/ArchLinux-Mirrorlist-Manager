b3a0355a6df3845675b6efc4d24ed7834b2468ae
Rizwan Hasan <rizwan.hasan486@gmail.com>
2021-01-14 18:43:25 +0600

    api fixed

--- mirrorlist.py
+++ mirrorlist.py
@@ -14,7 +14,7 @@ class Mirrorlist():
     def __init__(self):
         self.mirrorfile = "/tmp/mirrorlist.txt"
         self.country_code_dataset = self.appFolder + 'country_code.data'
-        self.url = "https://www.archlinux.org/mirrorlist/?COUNTRYCODEprotocol=http&protocol=https&ip_version=4"
+        self.url = "https://www.archlinux.org/mirrorlist/?country=COUNTRYCODE&protocol=http&protocol=https&ip_version=4"
 
     # Mirrorlist getting ↓
     def get_mirrorlist(self, country_code):
@@ -27,7 +27,7 @@ class Mirrorlist():
         country_code = self.country_code_detect(country_code)
 
         url = "'" + self.create_url(country_code) + "'"
-        subprocess.getoutput("curl -o " + self.mirrorfile + " " + url)
+        subprocess.getoutput("curl -L -o " + self.mirrorfile + " " + url)
 
         mirrordata = None
         with open(self.mirrorfile, 'r') as file:
